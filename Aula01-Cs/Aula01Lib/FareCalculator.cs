c8a7983630d6529b074ee8459f75f682af3a1663
CarlosEvandro-Fi <carlosevandro.focus@gmail.com>
2022-07-08 15:41:43 -0300

    Comentário removido.

--- Aula01-Cs/Aula01Lib/FareCalculator.cs
+++ Aula01-Cs/Aula01Lib/FareCalculator.cs
@@ -1,12 +1,5 @@
 ﻿namespace Aula01Lib;
 
-//public abstract class FareCalculator
-//{
-//    public abstract FareCalculator? Next { get; }
-
-//    public abstract decimal Calculate(Segment segment);
-//}
-
 public interface IFareCalculator
 {
     IFareCalculator? Next { get; }
