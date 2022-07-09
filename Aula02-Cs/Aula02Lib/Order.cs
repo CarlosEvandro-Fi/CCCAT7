a832b266bba5c977ecc7873f52ce11b3a820b078
Carlos Evandro <carlosevandro.focus@gmail.com>
2022-07-09 18:30:29 -0300

    Aula02

---
+++ Aula02-Cs/Aula02Lib/Order.cs
@@ -0,0 +1,26 @@
+﻿namespace Aula02Lib;
+
+public sealed class Order
+{
+    private List<OrderItem> Items { get; } = new();
+    private CPF CPF { get; }
+    private Coupon? Coupon { get; set; }
+
+    public Order(CPF cpf) => CPF = cpf;
+
+    public void AddItem(Item item, Int32 quantity)
+    {
+        Items.Add(new OrderItem(item.ItemId, item.Price, quantity));
+    }
+
+    public void AddCoupon(Coupon coupon) => Coupon = coupon;
+    
+    public Decimal GetTotal()
+    {
+        var total = Items.Sum(item => item.GetTotal());
+
+        total -= (Coupon is null ? 0 : Coupon.GetDiscount(total));
+
+        return total;
+    }
+}
