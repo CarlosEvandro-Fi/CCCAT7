9da7416995ae8b5bb1fb4b2efe39cc2cbe485354
Carlos Evandro <carlosevandro.focus@gmail.com>
2022-07-25 15:19:41 -0300

    Usando o memory repository.

--- Projeto-Parte-04-Cs/eCommerce.Infrastructure/Repository/Database/ItemRepositoryDatabase.cs
+++ Projeto-Parte-04-Cs/eCommerce.Infrastructure/Repository/Database/ItemRepositoryDatabase.cs
@@ -1,13 +1,18 @@
-﻿using eCommerce.Infrastructure.DB;
+﻿using eCommerce.Domain;
+using eCommerce.Infrastructure.DB;
 
 namespace eCommerce.Infrastructure.Repository.DB;
 
-public sealed class ItemRepositoryDatabase
+public sealed class ItemRepositoryDatabase : IItemRepository
 {
+    private Repository.Memory.ItemRepositoryMemory Memory { get; } = new();
+
     public IConnection Connection { get; }
 
     public ItemRepositoryDatabase(IConnection connection)
     {
         Connection = connection;
     }
+
+    public Task<Item> GetItem(int itemId) => Memory.GetItem(itemId);
 }
--- Projeto-Parte-04-Cs/eCommerce.Infrastructure/Repository/Database/OrderRepositoryDatabase.cs
+++ Projeto-Parte-04-Cs/eCommerce.Infrastructure/Repository/Database/OrderRepositoryDatabase.cs
@@ -1,13 +1,22 @@
-﻿using eCommerce.Infrastructure.DB;
+﻿using eCommerce.Domain;
+using eCommerce.Infrastructure.DB;
 
 namespace eCommerce.Infrastructure.Repository.DB;
 
-public sealed class OrderRepositoryDatabase
+public sealed class OrderRepositoryDatabase : IOrderRepository
 {
+    private Repository.Memory.OrderRepositoryMemory Memory { get; } = new();
+
     public IConnection Connection { get; }
 
     public OrderRepositoryDatabase(IConnection connection)
     {
         Connection = connection;
     }
+
+    public Task<Int32> Count() => Memory.Count();
+
+    public Task Save(Order order) => Memory.Save(order);
+
+    public Task Clean() => Memory.Clean();
 }
