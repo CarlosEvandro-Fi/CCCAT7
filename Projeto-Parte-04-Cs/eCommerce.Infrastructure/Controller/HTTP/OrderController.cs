10b2f18b4472d3da5823534787094769b1d1356f
Carlos Evandro <carlosevandro.focus@gmail.com>
2022-08-03 16:48:50 -0300

    Compilando.

--- Projeto-Parte-04-Cs/eCommerce.Infrastructure/Controller/HTTP/OrderController.cs
+++ Projeto-Parte-04-Cs/eCommerce.Infrastructure/Controller/HTTP/OrderController.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using eCommerce.Application;
+using eCommerce.Infrastructure.DB;
+using eCommerce.Infrastructure.HTTP;
+using eCommerce.Infrastructure.Repository.DB;
 
-namespace eCommerce.Infrastructure.Controller.HTTP
+namespace eCommerce.Infrastructure.Controller.HTTP;
+
+public sealed class OrderController
 {
-    internal class OrderController
+    public OrderController(IHttp http, IConnection connection)
     {
+        //http.on("post", "/orderPreview", function (params: any, body: any) {
+        //    const itemRepository = new ItemRepositoryDatabase(connection);
+        //    const previewOrder = new PreviewOrder(itemRepository);
+        //    const output = previewOrder.execute(body);
+        //    return output;
+        //});
+
+        //http.On("post", "/orderPreview", (String @params, PreviewOrder.Input body) => {
+        //    var itemRepository = new ItemRepositoryDatabase(connection);
+        //    var previewOrder = new PreviewOrder(itemRepository);
+        //    var output = previewOrder.Execute(body);
+        //    return output;
+        //});
     }
 }
---
+++ Projeto-Parte-04-Cs/eCommerce.Infrastructure/HTTP/IHttp.cs
@@ -0,0 +1,8 @@
+﻿namespace eCommerce.Infrastructure.HTTP;
+
+public interface IHttp
+{
+	void Listen(int port);
+
+    void On(string method, string url, Action callback);
+}
